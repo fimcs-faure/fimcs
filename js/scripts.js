b020bcdd66c952c83405b99c74fa9633d7ce4142
Pachalik <67580517+Pachalik@users.noreply.github.com>
2020-11-15 16:23:46 +0100

    Add files via upload

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,4 @@
+// Scroll et menu
 $(document).ready(function(){
   $(window).scroll(function(){
     if(this.scrollY > 20){
@@ -17,3 +18,31 @@ $(document).ready(function(){
     $(".navbar-menu").toggleClass("active");
   });
 });
+
+// Annonces flash :
+function hideFlash(){
+  document.getElementById('annonce-flash').style="display: none;"
+}
+setTimeout(() => {
+  hideFlash()
+}, 10000)
+
+// Titre responsive :
+
+function responsiveTitle(){
+  width = window.innerWidth;
+  console.log('Big Brothers watches you')
+  if (666>width){
+    document.getElementById('welcomeTitle').innerHTML="FIMCS"
+  }
+  else if (width==666){
+    document.getElementById('welcomeTitle').innerHTML="Satan est présent"
+  }
+  else if(width>850) {
+    document.getElementById('welcomeTitle').innerHTML="Forum Intellectuel de la Meilleure Classe de Seconde"
+  }
+}
+setInterval(responsiveTitle, 100);
+
+
+
